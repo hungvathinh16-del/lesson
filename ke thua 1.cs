e56ce47cc04477b0dd47237dabbec3b0be5ee3b3
phuc thinh <hungvathinh16@gmail.com>
2026-02-13 19:59:57 +0700

    hw

---
+++ ke thua 1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lesson
+{
+    internal class Menu
+    {
+        
+        static void Main(string[] args)
+        {
+            Animal Lu = new Dog("Lu");
+            Animal Mun = new Cat("Mun");
+
+            Lu.Keu();
+            Mun.Keu();
+        }
+
+        public class Animal
+        {
+            public string Ten { get; set; }
+
+            public Animal() { }
+            public Animal(string name) { this.Ten = name; }
+
+            public virtual void Keu()
+            {
+                Console.WriteLine("Động vật đang kêu...");
+            }
+        }
+
+        public class Dog : Animal
+        {
+            public Dog(string name) : base(name) { }
+            public override void Keu()
+            {
+                Console.WriteLine("Gâu gâu!");
+            }
+        }
+
+        public class Cat : Animal
+        {
+            public Cat(string name) : base(name) { }
+            public override void Keu()
+            {
+                Console.WriteLine("Meo Meo!");
+            }
+        }
+
+    }
+    
+    
+}
